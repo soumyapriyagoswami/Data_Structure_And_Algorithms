f273ae0588ae14d4075106544afb06b29bcfc92b
soumyahacker <pg2196577@gmail.com>
2025-07-18 23:38:42 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   Arrays/Kadane's Algorithm/Brute_Force.class 	new file:   Arrays/Kadane's Algorithm/Brute_Force.java 	new file:   Arrays/Kadane's Algorithm/problem.txt

---
+++ Arrays/Kadane's Algorithm/Brute_Force.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Brute_Force {
+    public static int maximum_SubArray(int[] array, int n){
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i<n; i++){
+            for(int j = i; j<n;j++){
+                int sum = 0;
+
+                for(int k = 0; k <= j; k++){
+                    sum += array[k];
+                }
+                max = Math.max(max,sum);
+            }
+        }
+
+        return max;
+    }
+
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        int maxSum = maximum_SubArray(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+}
