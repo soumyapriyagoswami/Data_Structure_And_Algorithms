655f41e50ed41fcbca319085da1b51369fb1116a
soumyahacker <pg2196577@gmail.com>
2025-09-07 02:09:50 +0530

    On branch main  Changes to be committed: 	new file:   Graphs/Rotten_Oranges/Solution.class 	new file:   Graphs/Rotten_Oranges/Solution.java 	new file:   Graphs/Rotten_Oranges/Solution_main.class

---
+++ Graphs/Rotten_Oranges/Solution.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution{
+    public static int rotten_oranges(int[][] grid){
+        if(grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh = 0;
+
+
+        // count the number of the fresh oranges
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 2){
+                    queue.offer(new int[]{i,j});
+                }
+                if(grid[i][j] != 0){
+                    count_fresh++;
+                }
+            }
+        }
+
+        if(count_fresh == 0) return 0;
+        int count_min = 0, cnt = 0;
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
+
+        // bfs sorting for initially rotten oranges
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            cnt += size;
+            for(int i = 0; i<size;i++){
+                int[] point = queue.poll();
+                for(int j = 0; j < 4; j++){
+                    int x = point[0]+dx[j];
+                    int y = point[1]+dy[j];
+
+
+                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y] == 0 || grid[x][y] == 2) continue;
+                    grid[x][y] = 2;
+                    queue.offer(new int[]{x,y});
+                }
+            }
+            if(queue.size() != 0){
+                count_min++;
+            }
+        }
+        return count_fresh == cnt?count_min:-1;
+    }
+}
+ class Solution_main {
+    public static void main(String[] args) {
+        int arr[][]={ {2,1,1} , {1,1,0} , {0,1,1} };
+        int rotting = Solution.rotten_oranges(arr);
+        System.out.println("Minimum Number of Minutes Required "+rotting);
+    }
+}
